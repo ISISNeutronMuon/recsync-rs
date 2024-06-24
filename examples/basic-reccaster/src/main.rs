d4610b3612ca5631d6d7b75edfe00549f37f2e3b
Aqeel <aqeel.alshafei@stfc.ac.uk>
2024-06-24 14:01:18 +0100

    Update basic-reccaster example to reflect changes in the library

--- examples/basic-reccaster/src/main.rs
+++ examples/basic-reccaster/src/main.rs
@@ -1,7 +1,12 @@
-use reccaster::Reccaster;
+use reccaster::{record::Record, Reccaster};
 
 #[tokio::main]
 async fn main() {
-    let mut caster = Reccaster::new().await;
+
+    let mut record = Record::new("DEV:PYRECASTER".to_string(), "ai".to_string());
+    record.properties.insert("recordDesc".to_string(), "P4P4ISIS PyRecaster".to_string());
+    let records: Vec<Record> = vec![record];
+
+    let mut caster = Reccaster::new(records).await;
     caster.run().await;
 }
