82afb2768df98659baeec66ba20c076f30431c1f
Aqeel AlShafei <aqeel.alshafei@stfc.ac.uk>
2024-10-01 12:14:00 +0100

    Add copyright header to wire workspace

--- wire/src/codec.rs
+++ wire/src/codec.rs
@@ -1,3 +1,10 @@
+// This file is part of Recsync-rs.
+// Copyright (c) 2024 UK Research and Innovation, Science and Technology Facilities Council
+//
+// This project is licensed under both the MIT License and the BSD 3-Clause License.
+// You must comply with both licenses to use, modify, or distribute this software.
+// See the LICENSE file for details.
+
 use bytes::{Buf, BufMut, BytesMut};
 use std::{io, mem::size_of};
 use tokio_util::codec::{Decoder, Encoder};
